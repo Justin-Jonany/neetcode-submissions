2676bee2b7a66c9f7e57c78c8b6df5ceb98149d5
Justin Jonany <121374676+Justin-Jonany@users.noreply.github.com>
2026-09-22 17:08:57 -0400

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        strs_dict = {}
+        for s in strs: # O(nklogk)  and space is O(nk)
+            identity = [0] * 26
+            for c in s:
+                identity[ord(c) - ord('a')] += 1
+            key = str(identity)
+            if str(key) in strs_dict:
+                strs_dict[key].append(s)
+            else:
+                strs_dict[key] = [s]
+        return list(strs_dict.values()) # O(n)
+        # strs_dict = {}
+        # for s in strs: # O(nklogk)  and space is O(nk)
+        #     sorted_s = ''.join(sorted(s)) # O(klogk)
+        #     if sorted_s in strs_dict:
+        #         strs_dict[sorted_s].append(s)
+        #     else:
+        #         strs_dict[sorted_s] = [s]
+        # return list(strs_dict.values()) # O(n)
