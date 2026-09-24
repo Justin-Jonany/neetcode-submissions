517d2e845c91ba4ac83629d74f10c9d8045465be
Justin Jonany <121374676+Justin-Jonany@users.noreply.github.com>
2026-09-24 16:57:29 -0400

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for s in strs:
+            for c in s:
+                encoded += f"{ord(c)} "
+            encoded += "_ "
+        return encoded.rstrip()
+            
+
+    def decode(self, s: str) -> List[str]:
+        letters = s.split(' ')
+        print(letters)
+        decoded = []
+        i = 0
+        while i < len(letters):
+            curr = []
+            char = letters[i]
+            if len(char) != 0:
+                while char != "_":
+                    curr.append(chr(int(char)))
+                    i += 1
+                    if i >= len(letters):
+                        break
+                    char = letters[i]
+                decoded.append(''.join(curr))
+            i += 1
+        return decoded
+
